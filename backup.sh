20880584fef5b2bea32a39e03285ed687490fa6b
ninpucho <tbaker78@gmail.com>
2022-11-13 20:09:59 -0500

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,19 +11,19 @@ BACKUP_FOLDER="$(date "+%F-%H%M%S")"
 mkdir '/tmp/$(BACKUP_FOLDER)'
 
 # use sqlite3 to create backup (avoids corruption if db write in progress)
-sqlite3 /db.sqlite3 ".backup '/tmp/$(BACKUP_FOLDER)/db.sqlite3'"
+sqlite3 /vault/db.sqlite3 ".backup '/tmp/$(BACKUP_FOLDER)/db.sqlite3'"
 
 # backup attachments folder
-cp -ar '/attachements' '/tmp/$(BACKUP_FOLDER)/'
+cp -ar '/vault/attachements' '/tmp/$(BACKUP_FOLDER)/'
 
 # backup rsa_key
-cp -a '/rsa_key.*' '/tmp/$(BACKUP_FOLDER)/'
+cp -a '/vault/rsa_key.*' '/tmp/$(BACKUP_FOLDER)/'
 
 # backup icon cache folder
-cp -ar '/icon_cache' '/tmp/$(BACKUP_FOLDER)/'
+cp -ar '/vault/icon_cache' '/tmp/$(BACKUP_FOLDER)/'
 
 # backuo sends folder
-cp -ar '/sends' '/tmp/$(BACKUP_FOLDER)/'
+cp -ar '/vault/sends' '/tmp/$(BACKUP_FOLDER)/'
 
 # tar up backup and encrypt with openssl and encryption key
 tar -czf - /tmp/$(BACKUP_FOLDER) | openssl enc -e -aes256 -salt -pbkdf2 -pass pass:${BACKUP_ENCRYPTION_KEY} -out /backups/${BACKUP_FILE}.tar.gz
